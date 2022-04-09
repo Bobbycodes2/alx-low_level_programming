ef0396f008d32a86f3f062c58fe1a433fc20484c
Bobbycodes2 <asikpochristian@gmail.com>
2022-04-09 08:29:34 -0700

    Another commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
- * main - prints phrase "Programming is like building a multilingual puzzle"
+/**
+ * main - Entry point 
  *
- * Return: 0 if exited properly, non zero otherwise
-*/
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
