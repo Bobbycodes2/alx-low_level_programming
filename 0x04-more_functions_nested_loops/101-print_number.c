31d5aa38bc049713feb90278997e0e62b7fab929
Bobbycode2 <--global>
2022-04-14 20:42:46 -0700

    last

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,33 +3,24 @@
 /**
  * print_number - prints an integer.
  * @n: input integer.
- * Return: no return.
+ * Return: Always 0.
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
+		n1 = -n;
+		_putchar('-');
+	}else
 	{
-		m = n;
-	}
-	
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count * = 10;
+		n1 = n;
 	}
 
-	for (; count >= 1; count /= 10)
+	if (n1 / 10)
 	{
-		_putchar(((m / count) % 10) + 48);
+		print_number(n1 / 10);
 	}
+	_putchar((n1 % 10) + '0');
 }
