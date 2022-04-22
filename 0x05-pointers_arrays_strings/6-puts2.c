8cab49b2b81e52f20ababf114e301930ad3daf88
Bobbycode2 <--global>
2022-04-21 21:11:24 -0700

    Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * aracter of a string
+ * puts2 - prints every character of a string
  * @str: A pointer to an int that will be changed
  *
  *Return: void which means it is correct
